b05796768874ee9a22e33e1946812f986e805d41
Fershad Irani <27988517+fershad@users.noreply.github.com>
2023-02-26 15:14:13 +0800

    check multiple links at once

--- src/.eleventy.js
+++ src/.eleventy.js
@@ -1,16 +1,18 @@
 const greenLinks = require('./lib/greenLinks');
 
+const greenLinksPlugin = (eleventyConfig) => {
+}
+
 module.exports = (eleventyConfig) => {
-  eleventyConfig.namespace('green-links', () => {
-    eleventyConfig.addTransform('green-links', (content, outputPath) => {
-      try {
-        if (outputPath && outputPath.endsWith('.html')) {
-          content = greenLinks.greenLinks(content);
+    eleventyConfig.addTransform('green-links', async (content, outputPath) => {
+        try {
+          if (outputPath && outputPath.endsWith('.html')) {
+            content = await greenLinks(content);
+          }
+        } catch (error) {
+          console.error(error);
         }
-      } catch (error) {
-        console.error(error);
-      }
-      return content;
-    });
-  });
+        return content;
+      });
+    eleventyConfig.addPlugin(greenLinksPlugin);    
 };
--- src/lib/greenLinks.js
+++ src/lib/greenLinks.js
@@ -1,30 +1,32 @@
-import { parseHTML } from 'linkedom';
-import { hosting } from '@tgwf/co2'
-
+const { parseHTML } = require('linkedom');
+const { hosting } = require('@tgwf/co2');
 /**
  * Finds unsafe anchor tags and adds safer attributes
  * @param html The HTML string
  * @returns The parsed HTML with green checked anchor tags
  */
-const greenLinks = (html) => {
+const greenLinks = async (html) => {
   const { document } = parseHTML(html);
   const links = [...document.querySelectorAll('a')];
 
   if (links.length > 0) {
-    links.map((link) => {
-      if (/^(https?\:\/\/|\/\/)/i.test(link.href)) {
-        // Get the hostname from the link
-        const hostname = new URL(link.href).hostname;
-        hosting.check(hostname).then((result) => {
-            if (result) {
+      //   Get all unique hostnames from the links
+      const allValidLinks = links.filter((link) => /^(https?\:\/\/|\/\/)/i.test(link.href));
+      const uniqueHostnames = [...new Set(allValidLinks.map((link) => new URL(link.href).hostname))];
+
+        //   Check if the hostnames are green
+        const greenHostnames = await hosting.check(uniqueHostnames);
+
+        console.log(greenHostnames);
+        //   Add the green attribute to the links
+        allValidLinks.forEach((link) => {
+            const hostname = new URL(link.href).hostname;
+            if (greenHostnames.includes(hostname)) {
                 link.setAttribute('data-green-link', 'true');
             } else {
                 link.setAttribute('data-green-link', 'false');
             }
-        });     
-      }
-      return link;
-    });
+        });
   } else {
     return html;
   }
