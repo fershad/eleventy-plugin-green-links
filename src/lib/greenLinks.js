244b551a6ec8f7e7dcea817ca4cd4d062d68d91a
Fershad Irani <27988517+fershad@users.noreply.github.com>
2023-02-28 21:23:42 +0800

    remove false attribute

--- src/lib/greenLinks.js
+++ src/lib/greenLinks.js
@@ -36,8 +36,6 @@ function greenLinksSetup(config = {}) {
                         const hostname = new URL(link.href).hostname;
                         if (greenHostnames.includes(hostname)) {
                             link.setAttribute('data-green-link', 'true');
-                        } else if (!ignore.includes(hostname)) {
-                            link.setAttribute('data-green-link', 'false');
                         }
                     });
               } else {
